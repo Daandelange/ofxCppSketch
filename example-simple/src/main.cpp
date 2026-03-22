6beba49528852b1975050c776908399bbeca33fb
daan <idaany@gmail.com>
2026-03-22 15:14:17 +0100

    Comment forced values in example

--- example-simple/src/main.cpp
+++ example-simple/src/main.cpp
@@ -3,10 +3,10 @@
 
 // You can set these variables to customise the compiler
 // Be sure to set them before any "ofxCppSketch.cpp"
-#define OFXCPPSKETCH_CUSTOM_COMPILER "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++"
-#define OFXCPPSKETCH_CUSTOM_STD_VERSION " -std=c++17"
-#define OFXCPPSKETCH_CUSTOM_MAC_SDK "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk"
-#define OFXCPPSKETCH_CUSTOM_EXTRA_FLAGS " -O0 -target x86_64-apple-macosx10.15-macho -pipe -fexceptions -fvisibility=default -Wno-unused-parameter -Werror=return-type -fPIC -DDEBUG -DGCC_HAS_REGEX"
+//#define OFXCPPSKETCH_CUSTOM_COMPILER "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++"
+//#define OFXCPPSKETCH_CUSTOM_STD_VERSION " -std=c++17"
+//#define OFXCPPSKETCH_CUSTOM_MAC_SDK "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk"
+//#define OFXCPPSKETCH_CUSTOM_EXTRA_FLAGS " -O0 -target x86_64-apple-macosx10.15-macho -pipe -fexceptions -fvisibility=default -Wno-unused-parameter -Werror=return-type -fPIC -DDEBUG -DGCC_HAS_REGEX"
 
 #include "ofxCppSketch.h"
 
